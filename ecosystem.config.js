dd57653fb470b75f49e11840a64976628c0f4941
Erik Bogdan <erik.bogdan@verycreatives.com>
2025-11-13 07:37:07 +0100

    Modified ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,8 @@ module.exports = {
       {
         name      : 'tracking',
         cwd       : '/home/tracking',
-        script    : 'bun',
+        script    : 'pnpm',
+        interpreter: '/root/.nvm/versions/node/v20.19.5/bin/node',
         args      : ['run', 'start', '-p', '3666'],
         env: {
           NODE_ENV: 'production',
